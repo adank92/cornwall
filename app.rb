e04c4904b2970dafc769645be56ee3874485aa6d
Adan Koch <adank92@gmail.com>
2014-09-01 14:39:22 -0300

    Style and Back modifications

--- app.rb
+++ app.rb
@@ -3,8 +3,10 @@ require 'soundcloud'
 require 'json'
 require 'haml'
 
+CLIENT_ID = 'a2340d5b7b5f7e58128486190268ce71'
+
 def get_tracks(offset)
-	client = Soundcloud.new(:client_id => 'a2340d5b7b5f7e58128486190268ce71')
+	client = Soundcloud.new(:client_id => CLIENT_ID)
 
 	tracks = client.get('/tracks', :limit => 10, :offset => offset, :genres => 'jazz', :licence => 'cc-by-sa', :"duration[from]" => 150000, :"duration[to]" => 480000)
 end
@@ -24,20 +26,21 @@ get '/' do
 end
 
 post '/getTracksUrl' do
+	used_ids = params[:used_ids] || []
 	begin
 		offset = offset ? offset + 10 : 0
 		tracks = get_tracks(offset)
 		stream_urls ||= []
 		stream_urls  = 	tracks.inject(stream_urls) do |acc, track|
-						   acc << {:title => track.title, :mp3 => track.stream_url} if track.streamable
+						   acc << {:title => track.title, :mp3 => track.stream_url, :id => track.id} if track.streamable && !used_ids.include?(track.id.to_s)
 						   acc
 						end
 	end while stream_urls.count < 5
 
 	stream_urls.collect! do |track|
-		track[:mp3] += "?client_id=a2340d5b7b5f7e58128486190268ce71"
+		track[:mp3] += "?client_id=#{CLIENT_ID}"
 		track
 	end
 
-	stream_urls.to_json
+	stream_urls.slice!(0,5).to_json
 end
